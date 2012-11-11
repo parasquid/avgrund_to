847602f97d90e6d8a6509f03c1a3de3ddd8496c0
Tristan <tristan.gomez@gmail.com>
2012-11-11 20:23:27 +0800

    add a spinner

--- app/assets/javascripts/avgrund_to/avgrund.js
+++ app/assets/javascripts/avgrund_to/avgrund.js
@@ -1,3 +1,4 @@
+//= require_tree .
 /*!
  * avgrund 0.1
  * http://lab.hakim.se/avgrund
@@ -54,6 +55,25 @@ jQuery(function(){
 
     removeClass( container, 'avgrund-active' );
     jQuery('.modal-body').html('<div id=\"spinner\"><\/div>')
+    var opts = {
+      lines: 13, // The number of lines to draw
+      length: 7, // The length of each line
+      width: 4, // The line thickness
+      radius: 10, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      color: '#000', // #rgb or #rrggbb
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: true, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: 'auto', // Top position relative to parent in px
+      left: 'auto' // Left position relative to parent in px
+    };
+    var target = document.getElementById('spinner');
+    var spinner = new Spinner(opts).spin(target);
   }
 
   function disableBlur() {
